5ce5cad1a8fe2b7264ae621044befcdf7530b9d0
aghussb <aghussb03@gmail.com>
2025-02-19 17:52:56 +0700

    test : tested again notifier

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,18 +1,21 @@
 return {
-  -- "j-hui/fidget.nvim",
-  -- config = function()
-  -- 	require("fidget").setup({
-  -- 		notification = {
-  -- 			override_vim_notify = true, -- Automatically override vim.notify() with Fidget
-  -- 		},
-  -- 		-- window = { blend = 0 },
-  -- 	})
-  -- end,
- 
-  "vigoux/notifier.nvim",
-  config = function()
-    require("notifier").setup({
-      -- You configuration here
-    })
-  end,
+	-- "j-hui/fidget.nvim",
+	-- config = function()
+	-- 	require("fidget").setup({
+	-- 		notification = {
+	-- 			override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+	-- 		},
+	-- 		-- window = { blend = 0 },
+	-- 	})
+	-- end,
+
+	"vigoux/notifier.nvim",
+	config = function()
+		require("notifier").setup({
+			components = { -- Order of the components to draw from top to bottom (first nvim notifications, then lsp)
+				"nvim", -- Nvim notifications (vim.notify and such)
+				"lsp", -- LSP status updates
+			},
+		})
+	end,
 }
